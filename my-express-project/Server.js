dc9c6e64e84e4a4847d24b4728eac422e999e259
AlirezaSharif1995 <144997350+AlirezaSharif1995@users.noreply.github.com>
2024-04-27 14:28:31 +0430

    Add files

--- my-express-project/Server.js
+++ my-express-project/Server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const registrationRouter = require('./registrationRouter');
-const loginRouter = require('./LoginRouter'); 
+const loginRouter = require('./LoginRouter');
 const friendHandler = require('./friendHandler');
 
 const app = express();
@@ -10,8 +10,13 @@ const PORT = 3030;
 app.use(bodyParser.json());
 
 app.use('/api/auth/register', registrationRouter);
-app.use('/api/auth/login', loginRouter); 
-app.use('/api/friendHandler', friendHandler); 
+app.use('/api/auth/login', loginRouter);
+app.use('/api/friendHandler', friendHandler);
+
+
+app.get('/test', (req, res) => {
+  res.status(200).json({ message: 'Connected successful....' });
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
