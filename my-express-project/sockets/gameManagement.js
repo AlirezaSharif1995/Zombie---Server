e2dad61813e9cff45a1a1e64f41913d5db5b43e2
AlirezaSharif1995 <144997350+AlirezaSharif1995@users.noreply.github.com>
2024-05-11 15:31:21 +0430

    Update gameManagement.js

--- my-express-project/sockets/gameManagement.js
+++ my-express-project/sockets/gameManagement.js
@@ -1,3 +1,5 @@
+const connectedSockets = new Set();
+
 let gameState = {
     players: {},
     zombies: {}
@@ -5,7 +7,7 @@ let gameState = {
 
 const zones = {
     zone1: {
-        zombies: [], 
+        zombies: [],
     },
     zone2: {
         zombies: [],
@@ -17,32 +19,45 @@ const zones = {
 
 
 function handlePlayer(data, socket) {
+
     const { playerId } = data;
-    
+
     if (gameState.players[playerId]) {
 
-        gameState.players[playerId].position = data.position;
-        gameState.players[playerId].rotation = data.rotation;
+        gameState.players[playerId].posX = data.posX;
+        gameState.players[playerId].posY = data.posY;
+        gameState.players[playerId].posZ = data.posZ;
+        gameState.players[playerId].rotX = data.rotX;
+        gameState.players[playerId].rotY = data.rotY;
+        gameState.players[playerId].rotZ = data.rotZ;
         gameState.players[playerId].animationCode = data.animationCode;
         gameState.players[playerId].health = data.health;
 
     } else {
-        
+
         gameState.players[playerId] = {
-            position: data.position,
-            rotation: data.rotation,
+            username: data.username,
+            posX: data.posX,
+            posY: data.posY,
+            posZ: data.posZ,
+            rotX: data.rotX,
+            rotY: data.rotY,
+            rotZ: data.rotZ,
             animationCode: data.animationCode,
             health: data.health,
         };
-         }
-
-        emitEventToAllExcept(socket, 'playerMoved', {
-            playerId,
-            position: data.position,
-            rotation: data.rotation,
-            animationCode: data.animationCode,
-            health: data.health
-        });     
+    }
+    emitEventToAllExcept(socket, 'playerMoved', {
+        username: data.username,
+        posX: data.posX,
+        posY: data.posY,
+        posZ: data.posZ,
+        rotX: data.rotX,
+        rotY: data.rotY,
+        rotZ: data.rotZ,
+        animationCode: data.animationCode,
+        health: data.health
+    });
 }
 
 function handlePlayerShooting(data, socket) {
@@ -61,18 +76,19 @@ function handleZombieUpdate(zoneName, socket) {
 
 function handleMessage(message, socket) {
     try {
-        const data = JSON.parse(message);
+        // const data = JSON.parse(message);
+        const data = (message);
         const messageType = data.type;
 
         switch (messageType) {
             case 'playerMove':
-                handlePlayer(data,socket);
+                handlePlayer(data, socket);
                 break;
             case 'zombieUpdate':
-                handleZombieUpdate(data,socket);
+                handleZombieUpdate(data, socket);
                 break;
             case 'playerShooting':
-                handlePlayerShooting(data,socket);
+                handlePlayerShooting(data, socket);
                 break;
             default:
                 console.error('Unknown message type:', messageType);
@@ -82,29 +98,29 @@ function handleMessage(message, socket) {
     }
 }
 
-module.exports = function(io) {
-   
+module.exports = function (io) {
 
-io.on('connection', (socket) => {
 
-    console.log(` joined the chat`);
-  
-    socket.on('startGame',(username)=>{
-  
-      const playerId = 'player_' + Math.random().toString(36).substr(2, 9);
-      socket.playerId = playerId;
-      console.log('New player connected:', playerId);
-      socket.emit('playerJoined',username);
-    });
-    
-    socket.on('message', async (obj) => {
-          handleMessage(obj,socket);
-      });
-    
-    socket.on('close', () => {
-          console.log('Player disconnected:', playerId);
-          delete gameState.players[playerId];
-      });
+    io.on('connection', (socket) => {
+
+        connectedSockets.add(socket);
+
+        socket.on('startGame', (Entry) => {
+            const playerId = 'player_' + Math.random().toString(36).substr(2, 9);
+            socket.playerId = playerId;
+            console.log('New player connected:', playerId);
+            socket.emit('playerJoined', Entry);
+        });
+
+        socket.on('message', (obj) => {
+            handleMessage(obj, socket);
+        });
+
+        socket.on('close', () => {
+            console.log('Player disconnected:', playerId);
+            connectedSockets.delete(socket);
+            delete gameState.players[playerId];
+        });
     });
 }
 
@@ -118,11 +134,13 @@ function emitCurrentGameState(playerSocket) {
 }
 
 function emitEventToAllExcept(senderSocket, eventName, eventData) {
-    
-    senderSocket.clients.forEach((client) => {
-        if (client !== senderSocket) {
-            client.emit(eventName, eventData);
-        }
-    });
+
+    senderSocket.emit(eventName, eventData);
+
+    // connectedSockets.forEach((client) => {
+    //     if (client !== senderSocket) {
+    //         client.emit(eventName, eventData);
+    //     }
+    // });
 
 }
