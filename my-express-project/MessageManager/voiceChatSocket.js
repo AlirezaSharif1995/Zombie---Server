f326870d67b51528a351aca585010d9fc3b9a251
AlirezaSharifManataz <165467116+AlirezaSharifManataz@users.noreply.github.com>
2024-05-20 12:37:40 +0330

    no message

--- my-express-project/MessageManager/voiceChatSocket.js
+++ my-express-project/MessageManager/voiceChatSocket.js
@@ -2,9 +2,11 @@
     io.on('connection', (socket) => {
 
       socket.on('voice', (data) => {
-        // Broadcast the received voice data to all other clients except the sender
-        //socket.broadcast.emit('voice', data);
-        io.emit('voice', data);
+
+        // io.emit('voice',data);
+
+        socket.broadcast.emit('voice', data);
       });
+
     });
   };
