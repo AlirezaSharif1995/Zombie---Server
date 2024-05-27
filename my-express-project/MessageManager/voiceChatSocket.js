59be22bcae40c4fe360c4a010daec9be9b99388d
AlirezaSharifManataz <165467116+AlirezaSharifManataz@users.noreply.github.com>
2024-05-27 13:17:36 +0330

    no message

--- my-express-project/MessageManager/voiceChatSocket.js
+++ my-express-project/MessageManager/voiceChatSocket.js
@@ -1,13 +1,13 @@
   module.exports = function(io) {
     io.on('connection', (socket) => { 
       
-      socket.on('signal', (data) => {
-        io.to(data.to).emit('signal', data);
-    });
+    socket.on('voiceData', (data) => {
+      // Broadcast the data to all other clients
+      socket.broadcast.emit('voiceData', data);
+  });
 
   socket.on('disconnect', () => {
       console.log('user disconnected');
   });
-  
     });
   };
