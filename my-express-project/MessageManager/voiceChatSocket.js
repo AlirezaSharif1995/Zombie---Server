4fbd92839166f4ab75a86c45d8494f3b927a09cf
AlirezaSharifManataz <165467116+AlirezaSharifManataz@users.noreply.github.com>
2024-06-12 13:17:26 +0330

    +wrtc

--- my-express-project/MessageManager/voiceChatSocket.js
+++ my-express-project/MessageManager/voiceChatSocket.js
@@ -1,13 +1,15 @@
   module.exports = function(io) {
-    io.on('connection', (socket) => { 
-      
-    socket.on('voiceData', (data) => {
-      // socket.broadcast.emit('voiceData', data);
-      io.emit('voiceData', data)
-  });
-
-  socket.on('disconnect', () => {
-      console.log('user disconnected');
-  });
+    io.on('connection', (socket) => {
+      console.log('A user connected');
+  
+      socket.on('start_chat', (audioBytes) => {
+          // Handle the incoming audio bytes
+          console.log('Received audio data');
+          console.log(audioBytes)
+  
+          // Broadcast the audio data to other clients
+          io.emit('audio_data', audioBytes);
+          // socket.broadcast.emit('audio_data', audioBytes);
+      });
     });
   };
