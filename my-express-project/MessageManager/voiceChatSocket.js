58291f58f1e240d5750ccf28ce46d2ee5f30187f
AlirezaSharif1995 <144997350+AlirezaSharif1995@users.noreply.github.com>
2024-06-15 11:13:31 +0430

    Update voiceChatSocket.js

--- my-express-project/MessageManager/voiceChatSocket.js
+++ my-express-project/MessageManager/voiceChatSocket.js
@@ -1,12 +1,30 @@
+const mysql = require('mysql2/promise');
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: 'Alireza1995!',
+    database: 'zombie-City-database',
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+});
+  
   module.exports = function(io) {
     io.on('connection', (socket) => {
       connectedUsers[username] = socket.username;
   
-      socket.on('call',(fromName,toName)=>{
+      socket.on('call',async (fromName,toName)=>{
 
         try {
           io.to(connectedUsers[toName]).emit('call',fromName);
           console.log(`${fromName} calling to ${toName} ...`);
+
+          const time = new Date();
+          const timer = `${time.getHours()} , ${time.getMinutes()} , ${time.getDate()} , ${time.getMonth() +1 } , ${time.getFullYear()}`;
+          const randomToken = generateRandomToken();
+
+          await pool.query('INSERT INTO callLog (Id, sender, receiver, date) VALUES (?, ?, ?, ?)', [randomToken, fromName, toName, timer]);
+
         } catch (error) {
           console.log(error);
         }
@@ -44,4 +62,13 @@
       });
 
     });
+
   };
+
+  function generateRandomToken() {
+    let token = '';
+    for (let i = 0; i < 5; i++) {
+        token += Math.floor(Math.random() * 10); // Generate random digit (0-9)
+    }
+    return token;
+}
